59ffe2058a9eea3466b1c03d542c6be0dea2bdc4
@dekvillanueva <dekvillanueva@gmail.com>
2022-05-18 19:39:31 -0300

    Se agrega el footer a las vistas login.html y register.html, se agrega la vista cart.html se modifica app.js para que el servidor atienda GET request en localhost:3000/cart.html

--- app.js
+++ app.js
@@ -14,29 +14,35 @@ const httpRaiz = '/';
 const httpHome ='/home';
 const httpLogin ='/login';
 const httpRegister ='/register';
+const httpCart = '/cart';
 
 // HTML
 const homeHtml = 'home.html';
 const loginHtml = 'login.html';
 const registerHtml = 'register.html';
+const cartHtml = 'cart.html';
 
 // Define the static file path
 app.use(express.static(__dirname + '/public/'));
 
 app.get(httpRaiz, (req, res) => {
     res.sendFile(path.join(views, homeHtml))
-})
+});
 
 app.get(httpHome, (req,res) =>{
     res.sendFile(path.join(views, homeHtml))
-})
+});
 
 app.get(httpLogin, (req,res) =>{
     res.sendFile(path.join(views, loginHtml))
-})
+});
 
 app.get(httpRegister, (req,res) =>{
     res.sendFile(path.join(views, registerHtml))
+});
+
+app.get(httpCart, (req,res) =>{
+    res.sendFile(path.join(views, cartHtml))
 })
 
 app.listen(port, () =>{
