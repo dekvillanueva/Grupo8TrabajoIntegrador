ae9e1942d6da2fdd65018456c8c81044fa818de4
Mariano <marianomuchiut@gmail.com>
2022-06-20 13:34:29 -0300

    Se agrega la vista productDatail, con su ruta y método de controlador

--- controllers/manageProductsController.js
+++ controllers/manageProductsController.js
@@ -1,16 +1,20 @@
-
-
 const manageProductsController = {
-    manageProducts: (req, res) => {
-      res.render("manageProducts");
-    },
-    listProducts: (req, res) =>{
-      res.render("productDetail");
-    },
-    createProduct: (req, res) =>{
-      res.send(req.body.productName);
-      console.log(req.body);
-    }
-  };
-  
-  module.exports = manageProductsController;
+  manageProducts: (req, res) => {
+    res.render("manageProducts");
+  },
+  listProducts: (req, res) => {
+    res.render("productDetail");
+  },
+  createProduct: (req, res) => {
+    res.send(req.body.productName);
+    console.log(req.body);
+  },
+
+  detail: (req, res) => {
+    let id = req.params.id;
+    let producto = products.find((p) => p.id == id);
+    res.render("detail", { producto: producto });
+  },
+};
+
+module.exports = manageProductsController;
