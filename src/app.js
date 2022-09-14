e46845db4490f6800748c4733fd815f8472fe323
Mariano <marianomuchiut@gmail.com>
2022-09-14 09:30:57 -0300

    Se agrega error 404

--- src/app.js
+++ src/app.js
@@ -35,6 +35,10 @@ app.use('/', mainRoutes);
 app.use('/', productRouter);
 app.use('/', userRouter);
 
+app.use((req, res, next) => {
+    res.status(404).render('not-found')
+})
+
 
 
 app.listen(port, () =>{
